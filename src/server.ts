c57efce2ae8742f8557c17f4c9bd047f40612534
Mais Godagov <mgodagov@vk.com>
2026-01-21 22:56:31 +0300

    duwehgr

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
-import { createServer } from 'http';
+import { createServer } from "http";
 
-import { createApp } from './app/app';
-import { env } from './config/env';
+import { createApp } from "./app/app";
+import { env } from "./config/env";
 
 const app = createApp();
 const server = createServer(app);
 
 const PORT = env.port;
 
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Бэк работает на http://0.0.0.0:${PORT}`);
   console.log(`Local access: http://localhost:${PORT}`);
   console.log(`Network access: http://192.168.1.120:${PORT}`);
-  console.log(`🚀 Production deployment test - ${new Date().toISOString()}`);
+  console.log(
+    `🚀 Production deployment test mais - ${new Date().toISOString()}`,
+  );
 });
-
